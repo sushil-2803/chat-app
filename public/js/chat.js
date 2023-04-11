d21b461fe75c3bb52f6ad6eecaae9995de0dcff0
Sushil Dubey <sushilanildubey@gmail.com>
2023-04-11 23:44:55 +0530

    Socket.io Challenge

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,11 @@
-io()
+const socket =io()
+
+document.querySelector('#chat-app').addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const message=document.getElementById('message').value
+    socket.emit('sendMessage',message)
+})
+
+socket.on('message',(message)=>{
+    console.log(message)
+})
--- src/index.js
+++ src/index.js
@@ -10,9 +10,19 @@ const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
+
 io.on('connection',(socket)=>{
     console.log('New WebSocket connection')
+    socket.emit('message',"Welcome!")
+    // on is used to listen to a event
+    socket.on('sendMessage',(message)=>{
+        console.log(message)
+        //io sends message to all connceted to socket
+        //emit is used to send a message
+        io.emit('message',message)
+    })
 })
+
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
