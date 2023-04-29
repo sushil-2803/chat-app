e99d08a147b240a5e8a9d80637ff04910998330f
Sushil Dubey <sushilanildubey@gmail.com>
2023-04-30 01:56:32 +0530

    Form and Button States

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,7 +1,14 @@
 const socket =io()
 
-document.querySelector('#chat-app').addEventListener('submit',(e)=>{
+const $messageForm = document.querySelector('#chat-app')
+const $messageFormInput = $messageForm.querySelector('#message')
+const $messageFormButton = $messageForm.querySelector('#send')
+const $sendLocation = document.querySelector('#send-location')
+
+$messageForm.addEventListener('submit',(e)=>{
     e.preventDefault()
+
+    $messageFormButton.setAttribute('disabled','disabled')
     const message=document.getElementById('message').value
 
     // emit is used to emit an event to the server
@@ -9,6 +16,9 @@ document.querySelector('#chat-app').addEventListener('submit',(e)=>{
     // second param is the content, variable,
     // third param is ack for the completion of event - Not Complosury
     socket.emit('sendMessage',message, (error)=>{
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value=''
+        $messageFormInput.focus()
         if(error)
         {
             return console.log("Profanity not allowed")
@@ -23,14 +33,19 @@ socket.on('message',(message)=>{
     console.log(message)
 })
 
-document.getElementById('send-location').addEventListener('click',()=>{
+$sendLocation.addEventListener('click',()=>{
     if(!navigator.geolocation){
         return alert('Gelocation Not support by browser')
     }
+
+    $sendLocation.setAttribute('disabled','disabled')
     navigator.geolocation.getCurrentPosition((position)=>{
         socket.emit('sendLocation',{
             latitude:position.coords.latitude,
             longitude:position.coords.longitude
-        }, ()=>{console.log("Location Shared!")})
+        }, ()=>{
+            $sendLocation.removeAttribute('disabled')
+            console.log("Location Shared!")
+        })
     })
 })
